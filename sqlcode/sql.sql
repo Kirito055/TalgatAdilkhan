3aa15ec2e968e73131019cb91a0df9826b79c6d9
Talgat Baimaganbetov <talgat.baymaganbetov@mail.ru>
2021-03-03 20:46:28 +0600

    dutys

--- sqlcode/sql.sql
+++ sqlcode/sql.sql
@@ -1,29 +1,29 @@
-CREATE TABLE `javalastfinal`.`question`
-(
-    `question_id` BIGINT(10)   NOT NULL AUTO_INCREMENT,
-    `question`    TEXT         NOT NULL,
-    `answerA`     VARCHAR(255) NOT NULL,
-    `answerB`     VARCHAR(255) NOT NULL,
-    `answerC`     VARCHAR(255) NOT NULL,
-    `answerD`     VARCHAR(255) NOT NULL,
-    `right`       VARCHAR(255) NOT NULL,
-    PRIMARY KEY (`question_id`)
-) ENGINE = InnoDB;
-INSERT INTO `javalastfinal`.`question` (`question_id`, `question`, `answerA`, `answerB`, `answerC`, `answerD`, `right`) VALUES
-(1, 'Specify the directory name where the XML Layouts are stored', '/res/values', '/res/layout', '/res/values', '/target', '/res/layout'),
-(2, 'The root element of AndroidManifest.xml', ' service', 'app', ' manifest ', 'activity', 'manifest '),
-(3, 'What represents a single user interface screen?', 'View', 'Service', 'ViewGroup', 'Activity', 'Activity'),
-(4, 'All layout classes are a direct subclass of', 'Object', 'View', 'ViewGroup', 'AbsoluteLayout ', 'ViewGroup'),
-(5, 'A Layout that arranges its children in a single row or a single column.', 'Relative Layout', 'Frame layout', 'Linear Layout', 'Absolute Layout', 'Linear Layout'),
-(6, 'The basic building element of android user interface is called', 'View', 'ContentProvider', 'ViewGroup', 'Layout', 'View'),
-(7, 'Android is developed by', 'Android Inc', 'Apple Inc', 'Samsung', 'Google Inc ', 'Google Inc '),
-(8, 'Which daemon is used when the NameNode goes down?', 'HDFS', 'Yarn', 'DataNode', 'Secondary NameNode', 'Secondary NameNode'),
-(9, 'Which of the following method add a path or paths to the list of inputs?', 'addInputPath()', 'setInput()', 'setInputPaths()', 'addInput()', 'addInputPath()'),
-(10, 'Which of the file contains the configuration setting for HDFS daemons?', 'yarn-site.xml', 'hdfs-site.xml', ' mapred-site.xml', ' gradle-site.xml', 'hdfs-site.xml'),
-(11, 'Which configuration file is used to control the HDFS replication factor?', 'yarn-site.xml', 'mapred-site.xml', 'hdfs-site.xml', 'core-site.xml', 'hdfs-site.xml'),
-(12, 'Which of the following method used to set the output directory?', 'OutputFormat.setOutputpath()', 'FileOutputFormat.setOutputpath()', 'OutputFormat.setOutput()', 'FileOutputFormat.setOutput()', 'FileOutputFormat.setOutputpath()'),
-(13, 'Every android app must have AndroidManifest.xml file', 'True', 'False', 'I don\'t know', '', 'True'),
-(14, '________ object is used to invoke the next filter in the chain of filters?', 'Filter', 'Action', 'Config', 'FilterChain ', 'FilterChain ');
+# CREATE TABLE `javalastfinal`.`question`
+# (
+#     `question_id` BIGINT(10)   NOT NULL AUTO_INCREMENT,
+#     `question`    TEXT         NOT NULL,
+#     `answerA`     VARCHAR(255) NOT NULL,
+#     `answerB`     VARCHAR(255) NOT NULL,
+#     `answerC`     VARCHAR(255) NOT NULL,
+#     `answerD`     VARCHAR(255) NOT NULL,
+#     `right`       VARCHAR(255) NOT NULL,
+#     PRIMARY KEY (`question_id`)
+# ) ENGINE = InnoDB;
+# INSERT INTO `javalastfinal`.`question` (`question_id`, `question`, `answerA`, `answerB`, `answerC`, `answerD`, `right`) VALUES
+# (1, 'Specify the directory name where the XML Layouts are stored', '/res/values', '/res/layout', '/res/values', '/target', '/res/layout'),
+# (2, 'The root element of AndroidManifest.xml', ' service', 'app', ' manifest ', 'activity', 'manifest '),
+# (3, 'What represents a single user interface screen?', 'View', 'Service', 'ViewGroup', 'Activity', 'Activity'),
+# (4, 'All layout classes are a direct subclass of', 'Object', 'View', 'ViewGroup', 'AbsoluteLayout ', 'ViewGroup'),
+# (5, 'A Layout that arranges its children in a single row or a single column.', 'Relative Layout', 'Frame layout', 'Linear Layout', 'Absolute Layout', 'Linear Layout'),
+# (6, 'The basic building element of android user interface is called', 'View', 'ContentProvider', 'ViewGroup', 'Layout', 'View'),
+# (7, 'Android is developed by', 'Android Inc', 'Apple Inc', 'Samsung', 'Google Inc ', 'Google Inc '),
+# (8, 'Which daemon is used when the NameNode goes down?', 'HDFS', 'Yarn', 'DataNode', 'Secondary NameNode', 'Secondary NameNode'),
+# (9, 'Which of the following method add a path or paths to the list of inputs?', 'addInputPath()', 'setInput()', 'setInputPaths()', 'addInput()', 'addInputPath()'),
+# (10, 'Which of the file contains the configuration setting for HDFS daemons?', 'yarn-site.xml', 'hdfs-site.xml', ' mapred-site.xml', ' gradle-site.xml', 'hdfs-site.xml'),
+# (11, 'Which configuration file is used to control the HDFS replication factor?', 'yarn-site.xml', 'mapred-site.xml', 'hdfs-site.xml', 'core-site.xml', 'hdfs-site.xml'),
+# (12, 'Which of the following method used to set the output directory?', 'OutputFormat.setOutputpath()', 'FileOutputFormat.setOutputpath()', 'OutputFormat.setOutput()', 'FileOutputFormat.setOutput()', 'FileOutputFormat.setOutputpath()'),
+# (13, 'Every android app must have AndroidManifest.xml file', 'True', 'False', 'I don\'t know', '', 'True'),
+# (14, '________ object is used to invoke the next filter in the chain of filters?', 'Filter', 'Action', 'Config', 'FilterChain ', 'FilterChain ');
 
 CREATE TABLE `role`
 (
@@ -50,18 +50,22 @@ CREATE TABLE `user` (
                         `last_name` varchar(255) DEFAULT NULL,
                         `password` varchar(255) DEFAULT NULL,
                         `phone` varchar(255) DEFAULT NULL,
-                        `username` varchar(255) DEFAULT NULL,
+                        `username` varchar(255) DEFAULT NULL,`gender` VARCHAR(10) DEFAULT NULL,
+                        `group_id` BIGINT(20) DEFAULT NULL,
                         PRIMARY KEY (`user_id`),
-                        UNIQUE KEY `UK_ob8kqyqqgmefl0aco34akdtpe` (`email`)
+                        UNIQUE KEY `UK_ob8kqyqqgmefl0aco34akdtpe` (`email`),
+                        CONSTRAINT `FKa8m1smlfsc8kkjn2t6wpdmysk4545` FOREIGN KEY (`group_id`) REFERENCES `group` (`group_id`)
+
 ) ENGINE=InnoDB AUTO_INCREMENT=4 ;
-
-
-
+CREATE TABLE `javalastfinal`.`major` ( `major_id` BIGINT NOT NULL , `major_name` VARCHAR(255) NOT NULL , PRIMARY KEY (`major_id`), UNIQUE (`major_name`)) ENGINE = InnoDB;
+INSERT INTO `major` (`major_id`, `major_name`) VALUES ('0', 'Big Data'), ('1', ' SE'),('2', 'Cyber Security'), ('3', 'Computer Scinece');
+CREATE TABLE `javalastfinal`.`group` ( `group_id` BIGINT NOT NULL , `group` VARCHAR(22) NOT NULL , `major_id` BIGINT(20) NOT NULL , PRIMARY KEY (`group_id`), UNIQUE `group_unique` (`group`)
+                                     ,
+                                       CONSTRAINT `FKa8m1smlfsc8kkjn2t6wpdmysk4545sss` FOREIGN KEY (`major_id`) REFERENCES `major` (`major_id`)) ENGINE = InnoDB;
 LOCK TABLES `user` WRITE;
-INSERT INTO `user` VALUES (1,'dleskov@gmail.ru','','Dmitry','Leskov','$2a$12$DWCryEwHwbTYCegib92tk.VST.GG1i2WAqfaSwyMdxX0cl0eBeSve','5551112345','User'),(3,'asd@gmail.com','','alex','osipov','$2a$12$hZR7pcSf0JU/OTXR3TOyuu8r6C6n.JZE8Ei47E4LZs1t0Aq1AO6oC','1111111111','Admin');
-
+INSERT INTO `user` VALUES (1,'dleskov@gmail.ru','','Dmitry','Leskov','$2a$12$DWCryEwHwbTYCegib92tk.VST.GG1i2WAqfaSwyMdxX0cl0eBeSve','5551112345','User','Ono',1001),(3,'asd@gmail.com','','alex','osipov','$2a$12$hZR7pcSf0JU/OTXR3TOyuu8r6C6n.JZE8Ei47E4LZs1t0Aq1AO6oC','1111111111','Admin','Pacan',2);
 UNLOCK TABLES;
-
+INSERT INTO `group` (`group_id`, `group`, `major_id`) VALUES ('1001', 'SE-1911', '1'), ('1002', 'CS-1901', '2'),('1003', 'IT-1917', '3'), ('1004', 'IT-1914', '0');
 
 
 DROP TABLE IF EXISTS `user_role`;
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -15,7 +15,96 @@ public class User {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
     private String phone;
+    private String group;
+    private String gender;
+    private String major;
     private boolean enabled = true;
 
+    public long getUserId() {
+        return userId;
+    }
 
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
